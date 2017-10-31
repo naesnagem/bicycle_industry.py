6b01dc1e12f0af0dc1ac1591093202874edec04a
naesnagem <thisiswherespamgoesamigo@gmail.com>
2017-10-31 18:42:28 +0000

    Updated

--- bicycle_ind.py
+++ bicycle_ind.py
@@ -1,4 +1,5 @@
 import random
+import math
 from classes import Bicycle, Bike_Store, Customers
 
 if __name__ == '__main__':
@@ -7,7 +8,7 @@ if __name__ == '__main__':
     Create a bicycle shop that has 6 different bicycle models in stock. 
     The shop should charge its customers 20% over the cost of the bikes.
     """
-
+    
     bike_1 =Bicycle("Road Warrior", 5, 150, 30)
     bike_2 =Bicycle("Downhill Racer", 7, 250, 35)
     bike_3 =Bicycle("Steel Horse", 10, 375, 20)
@@ -15,8 +16,8 @@ if __name__ == '__main__':
     bike_5 =Bicycle("Speedy Gonzalez", 8, 800, 15)
     bike_6 =Bicycle("Bugs Bunny", 4, 125, 15)
 
-
-    showroom = [bike_1, bike_2, bike_3, bike_4, bike_5, bike_6 ]
+    showroom = [Bicycle("Road Warrior", 5, 150, 30), Bicycle("Downhill Racer", 7, 250, 35), Bicycle("Steel Horse", 10, 375, 20), 
+    Bicycle("Magic Carpet", 12, 650, 20), Bicycle("Speedy Gonzalez", 8, 800, 15), Bicycle("Bugs Bunny", 4, 125, 15)]
     store = Bike_Store("Good Bikes", showroom)
     
     """
@@ -34,27 +35,47 @@ if __name__ == '__main__':
     Print the name of each customer, and a list of the bikes offered by the bike shop that they can afford given their budget. 
     Make sure you price the bikes in such a way that each customer can afford at least one.
     """
-    for customer in range(len(customers)):
-        print("{} can afford these bikes:".format(customers[customer].name))
+    for customer in customers:
+        print("{} can afford these bikes:".format(customer.name))
         options = []
-        for bike in range(len(showroom)):
-            if showroom[bike].production_cost * 1.2 <= customers[customer].wallet:
-                options.append(store.inventory[bike].model)
+        for bike in showroom:
+            if bike.production_cost*1.2 <= customer.wallet:
+                options.append(bike.model)
         print(options)
-    
     
     """
     Print the initial inventory of the bike shop for each bike it carries.
     """
     
     print("\n{} has an initial inventory of these bikes:".format(store.name))
-    for bike in range(len(showroom)):
-        print(showroom[bike].model)
+    for bike in showroom:
+        print(bike.units, "units of the", bike.model, "model")
     
     """    
     Have each of the three customers purchase a bike then print the name of the bike the customer 
     purchased, the cost, and how much money they have left over in their bicycle fund.
     """
-    for customer in range(len(customers)):
-        print("\n{} has decided to buy:".format(customers[customer].name))
-        print(random.choice(options))
+    for customer in customers:
+        print("\nAfter considering the different options {} has decided to buy:".format(customer.name))
+        a = []
+        for bike in showroom:
+            if bike.production_cost * 1.2 <= customer.wallet:
+                a.append(bike)
+            b = random.choice(a)
+        print("The", b.model, "model which has a price of: {}".format(b.production_cost*1.2))
+        print("This leaves {} with:".format(customer.name))
+        print(customer.wallet - (b.production_cost*1.2), "after purchasing the bike")
+        print("This sale created a profit of {} for the bikeshop".format(b.production_cost*.2))
+        for bike in showroom:
+            if b.model == bike.model:
+                bike.units -= 1
+
+    """    
+    After each customer has purchased their bike, the script should print out the bicycle shop's 
+    remaining inventory for each bike, and how much profit they have made selling the three bikes.
+    """ 
+    print("\nHere is the updated inventory:")
+    for bike in showroom:
+        print(bike.model, bike.units)
+        
+    print("\nThe total profit generated from these sales is")
--- classes.py
+++ classes.py
@@ -4,29 +4,20 @@ class Bicycle(object):
         self.weight = weight
         self.production_cost = production_cost
         self.units = units
-
-
-class Bike_Store(object):
-    def __init__(self, name, inventory):
-        self.name = name
-        self.inventory = inventory
     
-    def in_budget(self, customer, margin = 1.2):
-        options = []
-        for bike in self.inventory:
-            if self.inventory[bike].production_cost * margin <= customers[customer].wallet:
-                options.append(self.inventory[bike].model)
-            print("{1} can afford to buy: {2}".format(customers[customer].name, options))
-        return options
-    
-    def sell(self, wallet, margin = 1.2):
-        self.wallet = wallet
-        choice = ()
-
-
 class Customers(object):
     def __init__(self, name, wallet ):
         self.name = name
         self.wallet = wallet
-        self.margin = 1.2
-        
+    
+
+class Bike_Store(object):
+    def __init__(self, name, margin = 1.2, inventory = {}, budget = {}, profit = 0):
+        self.name = name
+        self.margin = margin
+        self.inventory = inventory
+        self.budget = budget
+        self.profit = profit
+    
+
+
